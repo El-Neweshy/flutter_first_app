a3733af24785cc131a5f715dbe9b2206cdc12d6d
Mahmoud ElNeweshy <neweshy@Mahmouds-MacBook-Pro.local>
2023-02-14 08:34:38 +0200

    add setState

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  var questions = ['What is your name', 'what is your age'];
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questions = [
+    'What is your name?',
+    'what is your age?',
+    'What is your class?'
+  ];
+
+  var questionIndex = 0;
 
   void answerQuestion() {
-    print('answered');
+    setState(() {
+      questionIndex += 1;
+    });
+    print('done answered');
+  }
+
+  void resetQuestionIndex() {
+    setState(() {
+      questionIndex = 0;
+    });
+
+    print('reset done');
   }
 
   @override
@@ -14,15 +36,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My first App'),
+          title: Text('Questions App'),
         ),
         body: Column(
           children: [
-            Text('The question:'),
+            Text(questions[questionIndex]),
             ElevatedButton(child: Text('Answer1'), onPressed: answerQuestion),
             ElevatedButton(child: Text('Answer2'), onPressed: answerQuestion),
-            ElevatedButton(
-                child: Text('Answer3'), onPressed: () => print('yeah')),
+            ElevatedButton(child: Text('reset'), onPressed: resetQuestionIndex),
           ],
         ),
       ),
